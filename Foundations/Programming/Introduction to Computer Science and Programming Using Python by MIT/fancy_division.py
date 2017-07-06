ff954368bd66f31c1674c3b6440d1e2332e35310
FIXDiz <skafiamafia@gmail.com>
2017-07-06 14:28:03 +0100

    Add files via upload

---
+++ Foundations/Programming/Introduction to Computer Science and Programming Using Python by MIT/fancy_division.py
@@ -0,0 +1,10 @@
+def fancy_divide(list_of_numbers, index):
+   denom = list_of_numbers[index]
+   return [simple_divide(item, denom) for item in list_of_numbers]
+ 
+ 
+def simple_divide(item, denom):
+    try:
+        return item / denom
+    except ZeroDivisionError:
+        return 0
